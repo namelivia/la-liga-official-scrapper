e8fb75079b2496e39cb03ce4b4f1aa428687332b
José Ignacio Amelivia <ohcan2@gmail.com>
2019-11-30 21:33:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# La Liga Official Scrapper
+
+## Requeriments
+
+* python3
+* pip3
+
+## Installation
+
+Clone the project, navigate to its root folder and execute `pip3 install - e . --user` for installing it's dependencies.
+
+## Testing
+
+For executing the tests just execute `python3 -m unittest discover` on the project's root folder.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='LaLigaOfficialScrapper',
+    author='José Ignacio Amelivia Santiago',
+    author_email='jignacio.amelivia@gmail.com',
+    url='https://namelivia.com',
+    description='This is scrapper for La Liga Official wepage',
+    license='LICENSE',
+    long_description=open('README.md').read(),
+    packages=['la_liga_official_scrapper'],
+    include_package_data=True,
+    install_requires=[
+        'unidecode >= 1.1.1',
+        'pymongo >= 3.9.0',
+        'unittest2 >= 1.1.0',
+        'mongomock >= 3.18.0',
+    ],
+)
